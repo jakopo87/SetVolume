38940cd6a5929332ec2b2bd0afe837ecebb5a603
Jacopo Galati <jacopo.galati@outlook.com>
2018-07-05 21:24:31 +0200

    set the audio from cli

--- SetVolume/Program.cs
+++ SetVolume/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SetVolume
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+
+			double volume = defaultPlaybackDevice.Volume;
+			if (double.TryParse(args[0], out volume))
+			{
+				defaultPlaybackDevice.Volume = volume;
+			}
+			else
+			{
+				Console.WriteLine("Invalid value");
+			}
+
+
+		}
+	}
 }
