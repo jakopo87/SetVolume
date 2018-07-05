32f75ca07dbf4b4a8e565fd9717e3209ae24f6db
Jacopo Galati <jacopo.galati@outlook.com>
2018-07-05 21:03:15 +0200

    Aggiungere i file di progetto.

---
+++ SetVolume/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SetVolume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
